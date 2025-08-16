49d3c96e05869a9526c0df40af3acc42080b0994
Simon <18210921570@163.com>
2025-08-16 23:04:50 +0800

    style: 样式优化

--- src/views/dashboard/console/index.vue
+++ src/views/dashboard/console/index.vue
@@ -3,27 +3,13 @@
     <CardList></CardList>
 
     <el-row :gutter="20">
-      <el-col :sm="24" :md="12" :lg="10">
+      <el-col :sm="24" :md="12" :lg="12">
         <ActiveUser />
       </el-col>
-      <el-col :sm="24" :md="12" :lg="14">
+      <el-col :sm="24" :md="12" :lg="12">
         <SalesOverview />
       </el-col>
     </el-row>
-
-    <el-row :gutter="20">
-      <el-col :sm="24" :md="24" :lg="12">
-        <NewUser />
-      </el-col>
-      <el-col :sm="24" :md="12" :lg="6">
-        <Dynamic />
-      </el-col>
-      <el-col :sm="24" :md="12" :lg="6">
-        <TodoList />
-      </el-col>
-    </el-row>
-
-    <AboutProject />
   </div>
 </template>
 
@@ -31,10 +17,6 @@
   import CardList from './widget/CardList.vue'
   import ActiveUser from './widget/ActiveUser.vue'
   import SalesOverview from './widget/SalesOverview.vue'
-  import NewUser from './widget/NewUser.vue'
-  import Dynamic from './widget/Dynamic.vue'
-  import TodoList from './widget/TodoList.vue'
-  import AboutProject from './widget/AboutProject.vue'
   import { useCommon } from '@/composables/useCommon'
 
   defineOptions({ name: 'Console' })
